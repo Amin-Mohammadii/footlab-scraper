b543522cad3c72149f5215896c0ca05b116010d4
Amin-Mohammadii <amin.mohamadi.4529@gmail.com>
2025-11-25 01:36:23 +0330

    update
    change the request

--- scraping.py
+++ scraping.py
@@ -1,6 +1,8 @@
-import requests
+import cloudscraper
 from bs4 import BeautifulSoup
-standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
-data = requests.get(standings_url)
-print(data.text)
 
+scraper = cloudscraper.create_scraper()
+html = scraper.get("https://fbref.com/en/comps/9/Premier-League-Stats").text
+
+soup = BeautifulSoup(html, "html.parser")
+print(soup.prettify())
