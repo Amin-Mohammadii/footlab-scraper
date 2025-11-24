d65bd53e690459e455cbdd64580155a1fd7331e4
Amin-Mohammadii <amin.mohamadi.4529@gmail.com>
2025-11-25 02:13:16 +0330

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -2,7 +2,7 @@ import cloudscraper
 from bs4 import BeautifulSoup
 
 scraper = cloudscraper.create_scraper()
-html = scraper.get("https://fbref.com/en/comps/9/Premier-League-Stats").text
+html = scraper.get("https://fbref.com/en/").text
 
 soup = BeautifulSoup(html, "html.parser")
 print(soup.prettify())
