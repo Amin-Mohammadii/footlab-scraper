f5c455a9a1ff92a8bb5ff42413329351d3ba9014
Amin-Mohammadii <amin.mohamadi.4529@gmail.com>
2025-11-25 17:34:44 +0330

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -2,7 +2,7 @@ import cloudscraper
 from bs4 import BeautifulSoup
 
 scraper = cloudscraper.create_scraper()
-html = scraper.get("https://fbref.com/en/").text
+html = scraper.get("https://fbref.com").text
 
 soup = BeautifulSoup(html, "html.parser")
 print(soup.prettify())
