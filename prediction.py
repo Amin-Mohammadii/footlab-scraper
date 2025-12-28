adceeb606c0f6c290c992996cb78024da8050ff7
Amin-Mohammadii <amin.mohamadi.4529@gmail.com>
2025-12-28 17:22:46 +0330

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -10,7 +10,7 @@ matches["Opp_Code"] = matches["Opponent"].astype("category").cat.codes
 matches["Hour"] = matches["Time"].str.replace(":.+", "", regex=True).astype("int")
 matches["Day_Code"] = matches["Date"].dt.dayofweek
 matches["Target"]  = (matches["Result"] == "W").astype("int")
-rf = RandomForestClassifier(n_estimators=50,min_samples_split=10,random_state=1)
+rf = RandomForestClassifier(n_estimators=500,min_samples_split=500,random_state=1)
 train = matches[matches["Date"] < '2025-01-01']
 test = matches[matches["Date"] > '2025-01-01']
 predictors = ["Vnue_Code", "Opp_Code", "Hour", "Day_Code"]
@@ -34,7 +34,6 @@ group = roll(group, cols, new_cols)
 matches_roll = matches.groupby("team").apply(lambda x: roll(x, cols, new_cols))
 matches_roll = matches_roll.droplevel('team')
 matches_roll.index = range(matches_roll.shape[0])
-print(matches_roll)
 
 def make_predictions(matches , predictors ):
     train = matches[matches["Date"] < '2025-01-01']
@@ -44,11 +43,30 @@ def make_predictions(matches , predictors ):
     combined = pd.DataFrame(dict(actual=test["Target"], predicted=preds), index=test.index)
     P= precision_score(test["Target"], preds)
     return combined, P
-combined,P = make_predictions(matches_roll,predictors )
+combined,P = make_predictions(matches_roll,predictors + new_cols)
 print(P)
+print (matches_roll.columns)
+combined = combined.merge(matches_roll[["Date", "team","Opponent", "Result"]], left_index=True, right_index=True)
 print(combined)
 
 
 
 
+class MissingDict(dict):
+    __missing__ = lambda self, key: key
 
+map_values = {
+
+    "Brighton and Hove Albion": "Brighton",
+    "Manchester United": "Manchester Utd",
+    "Newcastle United": "Newcastle Utd",
+    "Tottenham Hotspur": "Tottenham",
+    "West Ham United": "West Ham",
+    "Wolverhampton Wanderers": "Wolves"
+}
+mapping = MissingDict(**map_values)
+combined["new_team"] = combined["team"].map(mapping)
+merged = combined.merge(combined, left_on=["Date", "new_team"], right_on=["Date", "Opponent"])
+print(merged[(merged["predicted_x"] == 1) & (merged["predicted_y"] ==0)]["actual_x"].value_counts()
+)
+print(merged)
